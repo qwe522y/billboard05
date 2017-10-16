52da3e8b63d34c618eddd4a7cc2bc8b897484371
artur <qwe522y@gmail.com>
2017-10-16 15:30:14 +0300

    Разрешил jpg как мини-изображение

--- src/main/java/com/sotas/billboard05/service/ImgServiceImpl.java
+++ src/main/java/com/sotas/billboard05/service/ImgServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -20,18 +19,10 @@ public class ImgServiceImpl implements ImgService{
 
     @Override
     public void setMiniImg(int bbId, byte[] data) throws IOException {
-        //png validation
-        if (data.length > 8 &&
-        data[0] == (byte) 0x89 &&
-        data[1] == (byte) 0x50 &&
-        data[2] == (byte) 0x4E &&
-        data[3] == (byte) 0x47 &&
-        data[4] == (byte) 0x0D &&
-        data[5] == (byte) 0x0A &&
-        data[6] == (byte) 0x1A &&
-        data[7] == (byte) 0x0A) {/* ok */}
-        else throw new ImgValidationErr("it's not png");
-        if(data.length > 500000) throw new ImgValidationErr("too big " + data.length);
+        if(data.length > 700000) throw new ImgValidationErr("too big " + data.length);
+        if (!pngValidation(data) && !jpgValidation(data)) {
+            throw new ImgValidationErr("it's not png");
+        }
         FileOutputStream fos = null;
         try {
             File bbPhotoDir = new File(photoDir + "/" + bbId);
@@ -49,11 +40,10 @@ public class ImgServiceImpl implements ImgService{
 
     @Override
     public void updateImg(int bbId, byte[] data, int index) throws IOException {
-        //jpg validation
-        if (data.length > 2 && data[0] == (byte) 0xFF && data[1] == (byte) 0xD8) { /* ok */ }
-        else throw new ImgValidationErr("it's not jpg");
-        if(data.length > 500000) throw new ImgValidationErr("too big " + data.length);
-
+        if(data.length > 700000) throw new ImgValidationErr("too big " + data.length);
+        if (!jpgValidation(data)) {
+            throw new ImgValidationErr("it's not jpg");
+        }
         FileOutputStream fos = null;
         try {
             File bbPhotoDir = new File(photoDir + "/" + bbId);
@@ -72,6 +62,22 @@ public class ImgServiceImpl implements ImgService{
         }
     }
 
+    private boolean jpgValidation(byte[] data) {
+        return data.length > 2 && data[0] == (byte) 0xFF && data[1] == (byte) 0xD8;
+    }
+
+    private boolean pngValidation(byte[] data) {
+        return data.length > 8 &&
+                data[0] == (byte) 0x89 &&
+                data[1] == (byte) 0x50 &&
+                data[2] == (byte) 0x4E &&
+                data[3] == (byte) 0x47 &&
+                data[4] == (byte) 0x0D &&
+                data[5] == (byte) 0x0A &&
+                data[6] == (byte) 0x1A &&
+                data[7] == (byte) 0x0A;
+    }
+
     @Override
     public void addImg(int bbId, byte[] data) throws IOException {
         updateImg(bbId, data, getImgCount(bbId));
@@ -81,12 +87,7 @@ public class ImgServiceImpl implements ImgService{
     public int getImgCount(int bbId) {
         File dir = new File(photoDir + "/" + bbId);
         if(!dir.exists()) return 0;
-        File[] list = dir.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                return pathname.getName().endsWith(".jpg");
-            }
-        });
+        File[] list = dir.listFiles(pathname -> pathname.getName().endsWith(".jpg"));
         return list == null ? 0 : list.length;
     }
 
